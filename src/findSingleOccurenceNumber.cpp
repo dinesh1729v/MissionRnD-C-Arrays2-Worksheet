246c4a3a41205d8bac776cfdea72598baaf548d6
dinesh1729v <dinesh1729v@missionrnd.com>
2016-01-10 12:25:13 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -13,6 +13,62 @@ ERROR CASES: Return -1 for invalid inputs.
 NOTES:
 */
 
+#include<stdio.h>
+#include<stdlib.h>
+void merge(int *arr, int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+	int *L=(int*)malloc(sizeof(int)*n1);
+	int *R = (int *)malloc(sizeof(int)*n2);
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+	i = 0;
+	j = 0;
+	k = l;
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+void mergeSort(int *arr, int l, int r)
+{
+	if (l < r)
+	{
+		int m = l + (r - l) / 2;
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+		merge(arr, l, m, r);
+	}
+}
 int findSingleOccurenceNumber(int *A, int len) {
-	return -1;
+	if (A == NULL || len < 0)return -1;
+	mergeSort(A, 0, len - 1);
+	for (int i = 0; i < len; i+=3)
+		if (A[i] != A[i + 1] || A[i] != A[i + 2])return A[i];
 }
